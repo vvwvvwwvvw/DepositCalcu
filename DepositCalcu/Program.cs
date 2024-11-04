9ec50675237091a030112aba2932d4543a30c2ed
남정욱 <wjddnr3304@gmail.com>
2024-11-04 10:04:42 +0900

    for문 예제

--- DepositCalcu/Program.cs
+++ DepositCalcu/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace DepositCalcu
 {
-     class Program
+    class Program
     {
         static void Main(string[] args)
         {
@@ -10,7 +10,9 @@
             int nDeposit;
             int period;
             double nCurrentDeposit;
-            int type = 1;
+            double currentDeposit1, currentDeposit2;
+
+            //int type = 1;
 
             Console.Write("저축 시작 년도를 입력 하세요");
             nStartYear = int.Parse(Console.ReadLine());
@@ -23,18 +25,28 @@
 
             Console.Write("이자율 :");
             interest = Double.Parse(Console.ReadLine());
-            period = nCurrentYear - nStartYear +1;
-            if (type == 1)
+            period = nCurrentYear - nStartYear + 1;
+
+            int i;
+            for (i = 1; i < period; i++)
             {
-                nCurrentDeposit = nDeposit * (1 + interest * period);
+                currentDeposit1 = nDeposit * (1 + interest * i);
+                currentDeposit2 = nDeposit * Math.Pow(1 + interest, i);
+                Console.WriteLine("{0}년도 예상 저축 금액 : 단리={1,2:F}원, 복리 = {2,2:F}원 입니다", nStartYear + i, currentDeposit1, currentDeposit2);
             }
-            else
-            {
-                nCurrentDeposit = nDeposit+Math.Pow(1 + interest,period
-                    );
-            }
-            Console.WriteLine("현재 잔액은 {0,2:F}원 입니다",nCurrentDeposit);
             Console.Read();
+            //    if (type == 1)
+            //    {
+            //        nCurrentDeposit = nDeposit * (1 + interest * period);
+            //    }
+            //    else
+            //    {
+            //        nCurrentDeposit = nDeposit+Math.Pow(1 + interest,period
+            //            );
+            //    }
+            //    Console.WriteLine("현재 잔액은 {0,2:F}원 입니다",nCurrentDeposit);
+            //    Console.Read();
+            //}
         }
     }
 }
